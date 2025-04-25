31a256afebb61f1a6b0865716d85cccbba9d428c
RickyReyes <77644172+RickyReyes@users.noreply.github.com>
2025-04-24 19:32:13 -0700

    all parts done, need to test it

--- FileTypeReport.cs
+++ FileTypeReport.cs
@@ -15,12 +15,10 @@ namespace FileTypeReport {
     // 1. Enumerate all files in a folder recursively
     private static IEnumerable<string> EnumerateFilesRecursively(string path) {
       // TODO: Fill in your code here.
-
       // Retrieve files at top level
       foreach (string file in System.IO.Directory.EnumerateFiles(path)) {
         yield return file;
       }
-
 
       // Recursively retrieve all nested files
       foreach (string directory in System.IO.Directory.EnumerateDirectories(path)) {
@@ -28,7 +26,6 @@ namespace FileTypeReport {
           yield return file;
         }
       }
-
       // END TODO
     }
 
@@ -53,18 +50,34 @@ namespace FileTypeReport {
       var query =
         from file in files
         // TODO: Fill in your code here.
+        let extension = Path.GetExtension(file) ?? ""
+        let size = new FileInfo(file).Length
+            group size by extension into g
+        let count = g.Count()
+        let totalSize = g.Sum()
+        orderby totalSize descending
         select new {
-          Type =      // TODO: Fill in your code here.
-          Count =     // TODO: Fill in your code here.
-          TotalSize = // TODO: Fill in your code here.
+          Type =      g.Key, // TODO: Fill in your code here.
+          Count =     count, // TODO: Fill in your code here.
+          TotalSize = totalSize // TODO: Fill in your code here.
         };
+
+        // END TODOS (4)
 
       // 3. Functionally construct XML
       var alignment = new XAttribute("align", "right");
       var style = "table, th, td { border: 1px solid black; }";
 
-      var tableRows = // TODO: Fill in your code here.
-        
+      // TODO: Fill in your code here.
+      var tableRows = query.Select(item =>
+                        new XElement("tr",
+                            new XElement("td", item.Type),
+                            new XElement("td", item.Count),
+                            new XElement("td", FormatByteSize(item.TotalSize))
+                        )
+                    )
+      // END TODO
+
       var table = new XElement("table",
         new XElement("thead",
           new XElement("tr",
